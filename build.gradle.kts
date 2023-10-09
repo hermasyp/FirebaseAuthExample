dfb07210351ba34341fab70a3f34fa891870bb0f
Muhammad Hermas Yuda Pamungkas <hermas.yuda@gmail.com>
2023-10-09 21:51:20 +0700

    Create starter point auth firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.0")
+        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
+    }
+}
 plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
