3c564297d655fb828a65942e8e659cd6467859c4
Muhammad Hermas Yuda Pamungkas <hermas.yuda@gmail.com>
2023-10-11 20:00:53 +0700

    Add Splash Screen

--- app/src/main/java/com/catnip/firebaseauthexample/presentation/splashscreen/SplashActivity.kt
+++ app/src/main/java/com/catnip/firebaseauthexample/presentation/splashscreen/SplashActivity.kt
@@ -1,10 +1,21 @@
 package com.catnip.firebaseauthexample.presentation.splashscreen
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.catnip.firebaseauthexample.data.network.firebase.auth.FirebaseAuthDataSource
+import com.catnip.firebaseauthexample.data.network.firebase.auth.FirebaseAuthDataSourceImpl
+import com.catnip.firebaseauthexample.data.repository.UserRepository
+import com.catnip.firebaseauthexample.data.repository.UserRepositoryImpl
 import com.catnip.firebaseauthexample.databinding.ActivitySplashBinding
+import com.catnip.firebaseauthexample.presentation.login.LoginActivity
+import com.catnip.firebaseauthexample.presentation.main.MainActivity
 import com.catnip.firebaseauthexample.utils.GenericViewModelFactory
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.lang.IllegalStateException
 
 class SplashActivity : AppCompatActivity() {
@@ -14,7 +25,10 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun createViewModel(): SplashViewModel {
-        return SplashViewModel()
+        val firebaseAuth = FirebaseAuth.getInstance()
+        val dataSource: FirebaseAuthDataSource = FirebaseAuthDataSourceImpl(firebaseAuth)
+        val repository: UserRepository = UserRepositoryImpl(dataSource)
+        return SplashViewModel(repository)
     }
 
     private val binding: ActivitySplashBinding by lazy {
@@ -25,13 +39,31 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         checkIfUserLogin()
-        throw IllegalStateException("")
     }
 
     private fun checkIfUserLogin() {
-        //todo : check user login
-        //if not login yet, navigate to login page
-        //if already login, navigate to main
+        lifecycleScope.launch {
+            delay(2000)
+            if (viewModel.isUserLoggedIn()) {
+                navigateToMain()
+            } else {
+                navigateToLogin()
+            }
+        }
+    }
+
+    private fun navigateToLogin() {
+        val intent = Intent(this, LoginActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        startActivity(intent)
+    }
+
+    private fun navigateToMain() {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        startActivity(intent)
     }
 
 
